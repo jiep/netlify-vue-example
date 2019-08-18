e665a15e3d7fdaeec057e5a8100a349f73bd9d70
José Ignacio Escribano <jiep12345@hotmail.com>
2019-08-18 21:07:52 +0200

    Add version 0.4.0

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,5 @@
     #nav.h-24.p-4.bg-blue-900.flex.items-center.justify-center.select-none
        p.text-white.text-3xl IACR eprint
     router-view.h-full
+    .text-xs.text-gray-500.absolute.top-0.right-0.p-2 Version 0.4.0
 </template>
