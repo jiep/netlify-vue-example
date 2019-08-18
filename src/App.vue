002645422cda889ec8aabb174efefa76c9728f3a
José Ignacio Escribano <jiep12345@hotmail.com>
2019-08-18 20:03:19 +0200

    Fix error

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  #app.h-screen.flex.flex-col.bg-gray-300.overscroll-none
+  #app.h-screen.flex.flex-col.bg-gray-300
     #nav.h-24.p-4.bg-blue-900.flex.items-center.justify-center.select-none
        p.text-white.text-3xl IACR eprint
     router-view.h-full
