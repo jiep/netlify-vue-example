85be8cc5f8dd9941ff39eb27a1dbcafa9ff465ab
José Ignacio Escribano <jiep12345@hotmail.com>
2019-08-18 17:54:16 +0200

    Fix errors and add dummy data

--- src-functions/papers.js
+++ src-functions/papers.js
@@ -1,6 +1,41 @@
-exports.handler = async (event, context) => {
+const osmosis = require('osmosis')
+const Bluebird = require('bluebird')
+
+const format = (paper) => {
+  const { title, authors, link } = paper
+  const link_url = link.startsWith('/') ? `https://eprint.iacr.org${link}` : link
   return {
-    statusCode: 200,
-    body: "Hello, World"
-  };
-};
+    title: `${title}`,
+    authors: `${authors}`,
+    link: `${link_url}`
+  }
+}
+
+const getLastWeekPapers = () => {
+  const url = 'https://eprint.iacr.org/eprint-bin/search.pl?last=7&title=1'
+
+  return new Bluebird((resolve, reject) => {
+    osmosis
+      .get(url)
+      .set({
+        'papers': [
+          osmosis
+            .find('body > dl > dt > a:nth-child(1)')
+            .follow('@href')
+            .find('body')
+            .set({
+              'title': 'b',
+              'authors': 'i',
+              'link': 'a@href'
+            })
+        ]
+      })
+      .data((listing) => resolve(listing.papers.map(p => format(p))))
+  })
+}
+
+getLastWeekPapers().then(x => console.log(x))
+
+// exports.handler = async (event, context) => {
+//   return (await getLastWeekPapers())
+// }
