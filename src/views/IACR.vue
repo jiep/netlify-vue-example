f60d77f44b7ea02549fe0550c914ef7fed644cfb
José Ignacio Escribano <jiep12345@hotmail.com>
2019-08-18 19:46:36 +0200

    Fix errors

--- src/views/IACR.vue
+++ src/views/IACR.vue
@@ -3,9 +3,9 @@
     .bg-blue-600.text-white.w-full.p-4.text-center.flex-grow-0.select-none
       .flex.justify-between.items-center
         p.text-xl.font-bold Last papers
-        button.bg-currrent.border-white.border.p-1.text-sm.rounded(class="hover:bg-blue-700", @click="refresh") Refresh
+        button.bg-currrent.border-white.border.p-1.text-sm.rounded(class="hover:bg-blue-700 focus:outline-none", @click="refresh") Refresh
     notification(:msg="msg", :type="type", v-if="msg || (isError && msg)")
-    papers-list(:papers="papers", :class="{spinner: isActive, 'opacity-50': isActive}")
+    papers-list.overscroll-none(:papers="papers", :class="{spinner: isActive, 'opacity-50': isActive}")
 </template>
 
 <script>
